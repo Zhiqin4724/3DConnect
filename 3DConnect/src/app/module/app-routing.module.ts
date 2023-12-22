7c5e6a1b3a57d9e3824429d6f8388156bca4ba50
Zhiqin <rzhiryin@hotmail.com>
2023-12-22 17:30:51 -0500

    seems to work

--- 3DConnect/src/app/module/app-routing.module.ts
+++ 3DConnect/src/app/module/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from '../../component/navbar/navbar.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'navbar', component: NavbarComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
--- 3DConnect/src/component/presentation/presentation.component.ts
+++ 3DConnect/src/component/presentation/presentation.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-presentation',
-  standalone: true,
-  imports: [],
   templateUrl: './presentation.component.html',
   styleUrl: './presentation.component.css'
 })
